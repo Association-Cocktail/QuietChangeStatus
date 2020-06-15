15f79a12b52a41607e836ad75e6efe9afdc87176
Marc-Antoine TURBET-DELOF <28108356+maturbet@users.noreply.github.com>
2020-06-15 12:27:42 +0200

    not tagged version

--- QuietChangeStatus.php
+++ QuietChangeStatus.php
@@ -17,7 +17,7 @@ class QuietChangeStatusPlugin extends MantisPlugin {
 		$this->description = plugin_lang_get( 'description' );    # Short description of the plugin
 		$this->page = 'config_page';           # Default plugin page
 
-		$this->version = '0.1.0';     # Plugin version string
+		$this->version = '0.0.2';     # Plugin version string
 		$this->requires = array(    # Plugin dependencies
 		    'MantisCore' => '2.24',  # Should always depend on an appropriate
 		                            # version of MantisBT
